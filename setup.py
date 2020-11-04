3d65a9353c421ffd52afc7e5aa55617c4198abb4
h0nda <41300123+davidbaszucki@users.noreply.github.com>
2020-11-04 09:02:30 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rockblox",
+    version="0.0.1",
+    author="h0nda",
+    author_email="1@1.com",
+    description="Roblox wrapper, with support for the game client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/davidbaszucki/rockblox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+    python_requires='>=3.6',
+)
