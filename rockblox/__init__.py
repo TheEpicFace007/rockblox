d7100e8caf1e1901c016ce32c7424fc123e6242e
h0nda <41300123+davidbaszucki@users.noreply.github.com>
2020-11-06 21:45:41 +0100

    Update __init__.py

--- rockblox/__init__.py
+++ rockblox/__init__.py
@@ -2,4 +2,4 @@ from .session import Session as Roblox
 from .client import Client as RobloxClient
 from .client import ClientMutex as RobloxClientMutex
 from .exceptions import WebError as RobloxWebError
-from .exceptions import ErrorType as RobloxWebErrorType
+from .exceptions import WebErrorType as RobloxWebErrorType
